09e4a557aa60ccbf3b0f37e00fa5c3eb2c9b756a
binishmaharjan <legendbinish124@gmail.com>
2019-06-01 01:19:35 +0900

    Add color set for xib

---
+++ Project_For_Binod/HSView.swift
@@ -0,0 +1,9 @@
+//
+//  HSView.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2019/06/01.
+//  Copyright © 2019 JEC. All rights reserved.
+//
+
+import Foundation
---
+++ Project_For_Binod/HasXib.swift
@@ -0,0 +1,9 @@
+//
+//  HasXib.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2019/05/31.
+//  Copyright © 2019 JEC. All rights reserved.
+//
+
+import Foundation
