3799f3ca827c7c1dfd491c895a94af11fc68cadc
binishmaharjan <legendbinish124@gmail.com>
2019-10-19 11:32:30 +0900

    add main view

---
+++ Project_For_Binod/MainView.swift
@@ -0,0 +1,13 @@
+//
+//  MainView.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2019/10/19.
+//  Copyright © 2019 JEC. All rights reserved.
+//
+
+import Foundation
+
+enum MainView {
+  
+}
