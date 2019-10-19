e930d678c9bc568cf696e2ef7fbcf22d06b716a6
binishmaharjan <legendbinish124@gmail.com>
2019-10-19 11:49:51 +0900

    add mainView types

--- Project_For_Binod/MainView.swift
+++ Project_For_Binod/MainView.swift
@@ -10,4 +10,23 @@ import Foundation
 
 enum MainView {
   
+  case launching
+  case onboarding
+  case signedIn(userProfile: UserProfile)
+}
+
+extension MainView {
+
+  static func ==(lhs: MainView, rhs: MainView) -> Bool {
+    switch (lhs, rhs) {
+    case (.launching, .launching):
+      return true
+      case (.onboarding, .onboarding):
+      return true
+    case let (.signedIn(l), .signedIn(r)):
+      return l == r
+    default:
+      return false
+    }
+  }
 }
