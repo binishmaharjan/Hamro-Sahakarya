2ae5c0ae7dd9f70b43dfebc5692c5d37461e9c18
binishmaharjan <legendbinish124@gmail.com>
2019-10-19 11:27:48 +0900

    adding nibless base controllers

---
+++ Project_For_Binod/NIblessViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NIblessViewController.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2019/10/19.
+//  Copyright © 2019 JEC. All rights reserved.
+//
+
+import UIKit
+
+open class NiblessViewController: UIViewController {
+
+  // MARK: - Methods
+  public init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  @available(*, unavailable,
+    message: "Loading this view controller from a nib is unsupported in favor of initializer dependency injection."
+  )
+  public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+
+  @available(*, unavailable,
+    message: "Loading this view controller from a nib is unsupported in favor of initializer dependency injection."
+  )
+  public required init?(coder aDecoder: NSCoder) {
+    fatalError("Loading this view controller from a nib is unsupported in favor of initializer dependency injection.")
+  }
+}
