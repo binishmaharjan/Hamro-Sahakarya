9cf3b22e22c079473c3c6cd0fa0d8d0e0b57e187
binishmaharjan <legendbinish124@gmail.com>
2019-10-19 11:54:32 +0900

    add Function to main view

--- Project_For_Binod/MainViewModel.swift
+++ Project_For_Binod/MainViewModel.swift
@@ -7,7 +7,29 @@
 //
 
 import Foundation
+import RxSwift
 
 class MainViewModel {
   
+  private let viewSubject = BehaviorSubject<MainView>(value: .launching)
+  var view: Observable<MainView> { return  viewSubject.asObservable() }
+  
+  init() { }
 }
+
+// MARK: Set Main Child View To SignedIn View
+extension MainViewModel: SignedInResponder {
+  
+  func signedIn(to userProfile: UserProfile) {
+    viewSubject.onNext(.signedIn(userProfile: userProfile))
+  }
+}
+
+// MARK: Set Main Child View To Onboarding View
+extension MainViewModel: NotSignedInResponder {
+  
+  func notSignedIn() {
+    viewSubject.onNext(.onboarding)
+  }
+}
+
