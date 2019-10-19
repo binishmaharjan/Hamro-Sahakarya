97cb813bb4cf89ca2ebe120aab0bf99a568e0b2a
binishmaharjan <legendbinish124@gmail.com>
2019-10-19 10:59:50 +0900

    add loggin method for debug and realease build

--- Project_For_Binod/Macros.swift
+++ Project_For_Binod/Macros.swift
@@ -13,3 +13,29 @@ let IS_DEBUG = true
 #else
 let IS_DEBUG = false
 #endif
+
+// MARK: Log For Debug
+/// Prints to console on debug build configuration
+func Dlog(_ obj: Any? = nil, file:String = #file, function:String = #function, line: Int = #line){
+  #if DEBUG
+  var filename:NSString = file as NSString
+  filename = filename.lastPathComponent as NSString
+  if let obj = obj{
+    print("[File:\(filename) Func:\(function) Line:\(line)] : \(obj)")
+  }else{
+    print("[File:\(filename) Func:\(function) Line:\(line)]")
+  }
+  #endif
+}
+
+// MARK: Log For Debug
+/// Prints to console on debug realease configuration
+func Alog(_ obj: Any? = nil, file: String = #file, function: String = #function, line: Int = #line){
+  var filename: NSString = file as NSString
+  filename = filename.lastPathComponent as NSString
+  if let obj = obj{
+    NSLog("[File:\(filename) Func:\(function) Line:\(line)] : \(obj)")
+  }else{
+    NSLog("[File:\(filename) Func:\(function) Line:\(line)]")
+  }
+}
