90cfa9820f5d07d29b753cae476a54a3a9203b62
binishmaharjan <legendbinish124@gmail.com>
2019-11-11 23:50:08 +0900

    extension to convert date to string and viceversa

---
+++ Project_For_Binod/Date+Extension.swift
@@ -0,0 +1,49 @@
+//
+//  Date+Extension.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2019/11/10.
+//  Copyright © 2019 JEC. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: Date To String
+extension Date {
+  
+  var toString: String {
+    let formatter = DateFormatter()
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+    return formatter.string(from: self)
+  }
+  
+}
+
+// MARK: String To Date
+extension String {
+  
+  var toDateTimeAndTime: Date {
+    let formatter = DateFormatter()
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+    if let date = formatter.date(from: self) {
+      return date
+    } else {
+      return Date()
+    }
+  }
+  
+  var toDate: Date {
+    let formatter = DateFormatter()
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd"
+    if let date = formatter.date(from: self) {
+      return date
+    } else {
+      return Date()
+    }
+  }
+  
+}
