282095cf4f47fcf663666ff216eea6a05a9cb787
binishmaharjan <legendbinish124@gmail.com>
2019-11-11 23:50:36 +0900

    uiview extension to edit view layer

---
+++ Project_For_Binod/UIView+Extension.swift
@@ -0,0 +1,31 @@
+//
+//  UIView+Extension.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2019/11/11.
+//  Copyright © 2019 JEC. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+  
+  @discardableResult
+  func cornerRadius(value radius: CGFloat = 2) -> UIView {
+    self.layer.cornerRadius = radius
+    return self
+  }
+  
+  @discardableResult
+  func borderWidth(value width: CGFloat = 1) -> UIView {
+    self.layer.borderWidth = width
+    return self
+  }
+  
+  @discardableResult
+  // TODO: Define Colors Class
+  func borderColor(color borderColor: UIColor = UIColor.orange) -> UIView {
+    self.layer.borderColor = borderColor.cgColor
+    return self
+  }
+}
