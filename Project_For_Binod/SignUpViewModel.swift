0e66c889af5099f99b92dd98d9be9d74144ed94f
binishmaharjan <legendbinish124@gmail.com>
2019-11-11 23:51:56 +0900

    added signupView Model class

---
+++ Project_For_Binod/SignUpViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2019/11/11.
+//  Copyright © 2019 JEC. All rights reserved.
+//
+
+import Foundation
+
+class SignUpViewModel {
+  
+}
