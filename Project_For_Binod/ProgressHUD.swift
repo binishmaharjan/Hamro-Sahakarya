d69d67661ef7b5dfd4e1ba934ba503a2553c5b93
binishmaharjan <legendbinish124@gmail.com>
2019-11-16 13:05:47 +0900

    Created progress hud

--- Project_For_Binod/ProgressHUD.swift
+++ Project_For_Binod/ProgressHUD.swift
@@ -6,4 +6,46 @@
 //  Copyright © 2019 JEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+class ProgressHUD: UIView {
+  
+  // MARK: IBOutlet
+  @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+  @IBOutlet private weak var statusText: UILabel!
+  
+  // MARK: Properties
+  var status: String = "Indicator" {
+    didSet {
+      statusText.text = status
+    }
+  }
+  
+  var isAnimating: Bool {
+    return activityIndicator.isAnimating
+  }
+  
+  // MARK: Instance
+  static func makeInstance() -> ProgressHUD {
+    let progressHud = ProgressHUD.loadXib()
+    return progressHud
+  }
+  
+  //MARK: Life Cycle
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    self.cornerRadius(value: 10)
+  }
+  
+  // MARK: Methods
+  func startAnimation() {
+    activityIndicator.startAnimating()
+  }
+  
+  func stopAnimation() {
+    activityIndicator.stopAnimating()
+  }
+}
+
+extension ProgressHUD: HasXib { }
