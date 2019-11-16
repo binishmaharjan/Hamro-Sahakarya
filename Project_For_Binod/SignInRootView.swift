b860d028452269ec12743ab68c0f41d02c7d4661
binishmaharjan <legendbinish124@gmail.com>
2019-11-16 13:06:14 +0900

    created sing in root view

--- Project_For_Binod/SignInRootView.swift
+++ Project_For_Binod/SignInRootView.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SignInRootView: UIView {
   
@@ -18,9 +20,11 @@ class SignInRootView: UIView {
   @IBOutlet private weak var passwordTextField: UITextField!
   
   @IBOutlet private weak var signInButton: UIButton!
+  @IBOutlet weak var scrollViewBottomContraints: NSLayoutConstraint!
   
   // MARK: Properties
   private var viewModel: SignInViewModel!
+  private let disposeBag = DisposeBag()
   
   // MARK: Instance
   static func makeInstance(viewModel: SignInViewModel) -> SignInRootView {
@@ -35,13 +39,66 @@ class SignInRootView: UIView {
     setupView()
   }
   
+  override func didMoveToWindow() {
+    super.didMoveToWindow()
+    bindViews()
+  }
+  
   // MARK: Methods
   private func setupView() {
     emailFieldView.borderColor().borderWidth().cornerRadius()
     passwordFieldView.borderColor().borderWidth().cornerRadius()
     signInButton.cornerRadius()
   }
+}
+
+// MARK: Bind
+extension SignInRootView {
   
+  private func bindViews() {
+    emailTextField.rx.text.asDriver()
+      .map { $0 ?? "" }
+      .drive(viewModel.emailInput)
+      .disposed(by: disposeBag)
+    
+    passwordTextField.rx.text.asDriver()
+      .map { $0 ?? "" }
+      .drive(viewModel
+        .passwordInput)
+      .disposed(by: disposeBag)
+    
+    signInButton.rx.tap.asDriver()
+      .drive(onNext:{ [weak self] _ in
+        self?.viewModel.signIn()
+      })
+      .disposed(by: disposeBag)
+    
+    viewModel.activityIndicatorAnimating
+      .asDriver(onErrorJustReturn: false)
+      .drive(ProgressHUDManager.rx.isAnimating(view: self))
+      .disposed(by: disposeBag)
+    
+    viewModel.signInButtonEnabled
+      .asDriver(onErrorJustReturn: true)
+      .drive(signInButton.rx.isEnabled)
+      .disposed(by: disposeBag)
+  }
+}
+
+// MARK: Keyboard
+extension SignInRootView {
+
+  func resetScrollViewContentInset() {
+    scrollViewBottomContraints.constant = 0
+  }
+
+  func moveContentForDismissKeyboard() {
+    resetScrollViewContentInset()
+  }
+  
+  func moveContent(forKeyboardFrame keyboardFrame: CGRect) {
+    scrollViewBottomContraints.constant = -keyboardFrame.height
+  }
 }
 
 extension SignInRootView: HasXib { }
