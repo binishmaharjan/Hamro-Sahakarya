a8b59a57a7517c9e9e0dd15c92dc4abdc75d272e
binishmaharjan <legendbinish124@gmail.com>
2020-01-01 02:36:57 +0900

    extension for storyboard

--- Project_For_Binod/StoryboardInstantiable.swift
+++ Project_For_Binod/StoryboardInstantiable.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 JEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol StoryboardInstantiable {
+  static var className: String { get }
+  static func loadFromStoryboard() -> Self
+}
+
+extension StoryboardInstantiable {
+  static var className: String {
+    return String(describing: Self.self)
+  }
+  
+  static func loadFromStoryboard() -> Self {
+    let storyboard = UIStoryboard(name: className, bundle: nil)
+    guard let viewController = storyboard.instantiateViewController(withIdentifier: className) as? Self else {
+      fatalError("Couldnot instantiate ViewController: \(className)")
+    }
+    
+    return viewController
+  }
+}
