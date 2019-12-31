f6455a5e9e1c2097c65c37ef973e76ceed350abd
binishmaharjan <legendbinish124@gmail.com>
2020-01-01 01:18:07 +0900

    implementing sign up view controller

--- Project_For_Binod/SignUpViewController.swift
+++ Project_For_Binod/SignUpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RxSwift
 
 protocol SignUpViewModelFactory {
   func makeSignUpViewModel() -> SignUpViewModel
@@ -14,8 +15,86 @@ protocol SignUpViewModelFactory {
 
 class SignUpViewController: NiblessViewController {
   
+  // MARK: Properties
+  private let viewModel: SignUpViewModel
+  private let disposeBag = DisposeBag()
+  private var signUpRootView: SignUpRootView {
+    return self.view as! SignUpRootView
+  }
+  
+  // MARK: Init
+  init(viewModelFactory: SignUpViewModelFactory) {
+    viewModel = viewModelFactory.makeSignUpViewModel()
+    super.init()
+  }
+  
+  // MARK: LifeCycle
+  override func loadView() {
+    view = SignUpRootView.makeInstane(viewModel: viewModel)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = UIColor.brown
+    bindAction()
   }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    addKeyboardObservers()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    removeObservers()
+  }
+  
+  private func bindAction() {
+    viewModel.event
+      .subscribe(onNext: { [weak self] event in
+        guard let self = self else { return }
+        
+        switch event {
+        case .signUpButtonTapped:
+          print("Sign Up Button Tapped")
+        case .backButtonPressed:
+          print("Back Button Tapped")
+          self.navigationController?.popViewController(animated: true)
+        }
+      }).disposed(by: disposeBag)
+  }
+}
+
+// MARK: Keyboard Notification
+extension SignUpViewController {
+  
+  func addKeyboardObservers() {
+    let notificationCenter = NotificationCenter.default
+    notificationCenter.addObserver(self,
+                                   selector: #selector(handleContentUnderKeyboard(notification:)),
+                                   name: UIResponder.keyboardWillHideNotification, object: nil)
+    notificationCenter.addObserver(self,
+                                   selector: #selector(handleContentUnderKeyboard(notification:)),
+                                   name: UIResponder.keyboardWillChangeFrameNotification,
+                                   object: nil)
+  }
+
+  func removeObservers() {
+    let notificationCenter = NotificationCenter.default
+    notificationCenter.removeObserver(self)
+  }
+
+  @objc func handleContentUnderKeyboard(notification: Notification) {
+
+    guard let userInfo = notification.userInfo,
+      let keyboardEndFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else  { return }
+    
+    switch notification.name {
+    case UIResponder.keyboardWillHideNotification:
+      signUpRootView.moveContentForDismissKeyboard()
+    default:
+      let convertedKeyboardEndFrame = view.convert(keyboardEndFrame.cgRectValue, to: view.window)
+      signUpRootView.moveContent(forKeyboardFrame: convertedKeyboardEndFrame)
+    }
+  }
+  
 }
