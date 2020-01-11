f36b8d495732480c23ec4e8cca6d3528584b9e33
binishmaharjan <legendbinish124@gmail.com>
2020-01-11 14:13:13 +0900

    implementing color picker view controller

--- Project_For_Binod/ColorPaletteViewModel.swift
+++ Project_For_Binod/ColorPaletteViewModel.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+enum ColorPaletteEvent {
+  case selectButtonTapped
+  case cancelButtonTapped
+}
+
+protocol ColorPaletteViewModelProtocol {
+  var selectedColorInput: BehaviorRelay<UIColor> { get }
+  var event: Observable<ColorPaletteEvent> { get }
+  var selectButtonTapped: Binder<Void> { get }
+  var cancelButtonTapped: Binder<Void> { get }
+}
+
+class ColorPaletteViewModel: ColorPaletteViewModelProtocol {
+  var selectedColorInput = BehaviorRelay<UIColor>(value: .mainOrange)
+  
+  private let eventSubject = PublishSubject<ColorPaletteEvent>()
+  var event: Observable<ColorPaletteEvent> {
+    return eventSubject.asObservable()
+  }
+  
+  var selectButtonTapped: Binder<Void> {
+    return Binder(eventSubject) { observer, _ in
+      observer.onNext(.selectButtonTapped)
+    }
+  }
+  
+  var cancelButtonTapped: Binder<Void> {
+    return Binder(eventSubject) { observer, _ in
+      observer.onNext(.cancelButtonTapped)
+    }
+  }
+  
+}
