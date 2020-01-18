67e6ac341c73b48953c3265f15d8352be0b54cff
binishmaharjan <legendbinish124@gmail.com>
2020-01-18 20:04:40 +0900

    implementing view controller

--- Project_For_Binod/SignedInViewController.swift
+++ Project_For_Binod/SignedInViewController.swift
@@ -7,11 +7,50 @@
 //
 
 import UIKit
+import RxSwift
 
 class SignedInViewController: NiblessViewController {
   
+  // MARK: Properties
+  private let viewModel: SignedInViewModel
+  private let disposeBag = DisposeBag()
+  
+  private let tabBar: UITabBarController
+  
+  // MARK: Init
+  init(viewModel: SignedInViewModel, tabBar: UITabBarController) {
+    self.viewModel = viewModel
+    self.tabBar = tabBar
+    super.init()
+  }
+  
+  // MARK: Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = UIColor.blue
+    subscribe(to: viewModel.view)
+  }
+  
+  
+  // MARK: Methods
+  private func subscribe(to observable: Observable<SignedInView>) {
+    observable.distinctUntilChanged()
+      .subscribe(onNext: { [weak self] view in
+        self?.present(signedInView: view)
+      }).disposed(by: disposeBag)
+  }
+}
+
+// MARK: Presentation
+extension SignedInViewController {
+  
+  private func present(signedInView: SignedInView) {
+    switch signedInView {
+    case .tabbar:
+      presentTabBar()
+    }
+  }
+  
+  private func presentTabBar() {
+    addFullScreen(childViewController: tabBar)
   }
 }
