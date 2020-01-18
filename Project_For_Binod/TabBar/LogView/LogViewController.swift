ce3510c8de17b088714c6060ccec135f1ccef24e
binishmaharjan <legendbinish124@gmail.com>
2020-01-18 20:03:14 +0900

    added dummy viewcontroller

---
+++ Project_For_Binod/TabBar/LogView/LogViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LogViewController.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/01/18.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+
+class LogViewController: UIViewController {
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+  
+  private func setup() {
+    title = nil
+    tabBarItem.image = UIImage(named: "icon_log")?.withRenderingMode(.alwaysOriginal)
+    tabBarItem.selectedImage = UIImage(named: "icon_log_h")?.withRenderingMode(.alwaysOriginal)
+    tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+  }
+}
---
+++ Project_For_Binod/TabBar/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/01/18.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+  
+  private func setup() {
+    title = nil
+    tabBarItem.image = UIImage(named: "icon_profile")?.withRenderingMode(.alwaysOriginal)
+    tabBarItem.selectedImage = UIImage(named: "icon_profile_h")?.withRenderingMode(.alwaysOriginal)
+    tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+  }
+}
