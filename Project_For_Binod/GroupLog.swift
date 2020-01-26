9b5f6a5f7ec3bbe72919de88ba7a5c42eabd0cec
binishmaharjan <legendbinish124@gmail.com>
2020-01-26 21:27:40 +0900

    editing modal

--- Project_For_Binod/GroupLog.swift
+++ Project_For_Binod/GroupLog.swift
@@ -15,9 +15,7 @@ enum GroupLogType: String, Codable {
   case left = "left"
   case loanGiven = "loan_given"
   case loanReturned = "loan_returned"
-  case loanRequested = "loan_requested"
   case monthlyFee = "monthly_fee"
-  case fee = "fee"
   case extra = "extra"
   case expenses = "expenses"
   
@@ -29,20 +27,22 @@ enum GroupLogType: String, Codable {
 
 struct GroupLog: Codable, Equatable {
   
-  let logId: String?
-  let dateCreated: String?
+  let logId: String
+  let dateCreated: String
   let logType: GroupLogType
-  let logCreator: String?
-  let logTarget: String?
+  let logCreator: String
+  let logTarget: String
   let amount: Int?
+  let reason: String?
   
   private enum CodingKeys: String, CodingKey {
     case logId = "log_id"
     case dateCreated = "date_created"
     case logType = "log_type"
     case logCreator = "log_creator"
-    case logTarget = "log_target"
+    case logTarget = "log_owner"
     case amount
+    case reason
   }
   
 }
