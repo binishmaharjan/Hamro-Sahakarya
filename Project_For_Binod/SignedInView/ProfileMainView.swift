34e2a8b926054d11c39e31622eb37ad060fe0999
binishmaharjan <legendbinish124@gmail.com>
2020-01-28 23:00:06 +0900

    adding files

---
+++ Project_For_Binod/SignedInView/ProfileMainView.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileMainView.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/01/28.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
---
+++ Project_For_Binod/TabBar/ProfileMainDependencyContainer.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileMainDependencyContainer.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/01/28.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
