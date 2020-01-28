015476b119f7e50041bd418a6d41baa5e69dbbd2
binishmaharjan <legendbinish124@gmail.com>
2020-01-28 23:02:20 +0900

    implementing profile main view model

--- Project_For_Binod/SignedInView/ProfileMainViewModel.swift
+++ Project_For_Binod/SignedInView/ProfileMainViewModel.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+typealias ProfileMainNavigationAction = NavigationAction<ProfileMainView>
+
+protocol ProfileMainViewModel {
+  var view: Observable<ProfileMainNavigationAction> { get }
+  func navigate(to view: ProfileMainView)
+  func uiPresented(profileView: ProfileMainView)
+}
+
+final class DefaultProfileMainViewModel: ProfileMainViewModel {
+  
+  private let _view = BehaviorSubject<ProfileMainNavigationAction>(value: .present(view: .profileView))
+  var view: Observable<ProfileMainNavigationAction> { return _view.asObservable() }
+}
+
+extension DefaultProfileMainViewModel {
+  func navigate(to view: ProfileMainView) {
+    _view.onNext(.present(view: view))
+  }
+  
+  func uiPresented(profileView: ProfileMainView) {
+    _view.onNext(.presented(view: profileView))
+  }
+}
