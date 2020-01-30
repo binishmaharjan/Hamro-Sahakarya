936fc615b198a8b70eee86c1ba0834e8209a162c
binishmaharjan <legendbinish124@gmail.com>
2020-01-30 23:12:19 +0900

    adding files

---
+++ Project_For_Binod/TabBar/ProfileView/ProfileTopCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTopCell.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/01/29.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Project_For_Binod/UIKit/UIImageView+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageView+Extension.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/01/29.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
