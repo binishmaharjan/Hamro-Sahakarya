49f4f615768417b616d20dcb501a7315a7b92cf0
binishmaharjan <legendbinish124@gmail.com>
2020-01-30 23:17:50 +0900

    adding table to profile view

--- Project_For_Binod/TabBar/ProfileView/ProfileViewController.swift
+++ Project_For_Binod/TabBar/ProfileView/ProfileViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class ProfileViewController: UIViewController {
   
-  init() {
-    super.init(nibName: nil, bundle: nil)
-  }
+  // MARK: IBOutlet
+  @IBOutlet private weak var tableView: UITableView!
   
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+  // MARK: Properties
+  private var viewModel: ProfileViewModel!
+  private let disposeBag = DisposeBag()
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .white
+    tableView.registerXib(of: ProfileTopCell.self)
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+}
+
+// MARK: Storyboard Instantiable
+extension ProfileViewController: StoryboardInstantiable {
+  static func makeInstance(viewModel: ProfileViewModel) -> ProfileViewController {
+    let viewController = loadFromStoryboard()
+    viewController.viewModel = viewModel
+    return viewController
+  }
+}
+
+// MARK: TableView Datasource
+extension ProfileViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return viewModel.numberOfSection
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.numberOfRows(in: section)
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let row = viewModel.row(for: indexPath)
+    
+    switch row {
+      
+    case .top(let viewModel):
+      let cell = tableView.dequeueCell(of: ProfileTopCell.self, for: indexPath)
+      cell.bind(viewModel: viewModel)
+      return cell
+    case .changePicture:
+      let cell = UITableViewCell()
+      cell.textLabel?.text = "Change Picture"
+      return cell
+    case .changePassword:
+      let cell = UITableViewCell()
+      cell.textLabel?.text = "Change Password"
+      return cell
+    case .members:
+      let cell = UITableViewCell()
+      cell.textLabel?.text = "Members"
+      return cell
+    }
+  }
+}
+
+// MARK: TableView Delegate
+extension ProfileViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return section == 0 ? 0 : 10
+  }
+  
+}
+
   }
 }
