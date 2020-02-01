bccd2a9c4c34fe8f4690eebbdda336830f207f21
binishmaharjan <legendbinish124@gmail.com>
2020-02-01 13:08:04 +0900

    footer view for the profile tab

--- Project_For_Binod/TabBar/ProfileView/ProfileFooterView.swift
+++ Project_For_Binod/TabBar/ProfileView/ProfileFooterView.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2020 JEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ProfileFooterView: UIView {
+  // MARK: IBOutlet
+  @IBOutlet weak var groupNameLabel: UILabel!
+  @IBOutlet weak var versionLabel: UILabel!
+  
+  // MARK: Properties
+  private var groupName: String? {
+    return Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String
+  }
+  
+  private var version: String {
+    guard let v = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+      return ""
+    }
+    return v
+  }
+  
+  // MARK: Lifecycle
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    setup()
+  }
+  
+  // MARK: Setup
+  private func setup() {
+    groupNameLabel.text = groupName
+    versionLabel.text = "v\(version)"
+  }
+  
+}
+
+// MARK: Instance
+extension ProfileFooterView: HasXib {
+  
+  static func makeInstance() -> ProfileFooterView {
+    let footerView = ProfileFooterView.loadXib()
+    return footerView
+  }
+}
+
