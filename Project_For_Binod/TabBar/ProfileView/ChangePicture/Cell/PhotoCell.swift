da480ecdb72ed97618cfbb1d21c4c5c307d10f88
binishmaharjan <legendbinish124@gmail.com>
2020-02-16 21:34:00 +0900

    photo cell for image picker

--- Project_For_Binod/TabBar/ProfileView/ChangePicture/Cell/PhotoCell.swift
+++ Project_For_Binod/TabBar/ProfileView/ChangePicture/Cell/PhotoCell.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class PhotoCell: UICollectionViewCell {
+final class PhotoCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+  @IBOutlet weak var photoView: UIImageView!
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    photoView.image = nil
+  }
+  
+  func bind(viewModel: PhotoCellViewModel) {
+    photoView.image = viewModel.image
+  }
+
+}
+
+extension PhotoCell {
+  
+  func flash() {
+    photoView.alpha = 0
+    setNeedsDisplay()
+    
+    UIView.animate(withDuration: 0.5) { [weak self] in
+      self?.photoView.alpha = 1
     }
+  }
+}
 
+protocol PhotoCellViewModel {
+  var image: UIImage? { get }
+}
+
+struct DefaultPhotoCellViewModel: PhotoCellViewModel {
+  let image: UIImage?
 }
