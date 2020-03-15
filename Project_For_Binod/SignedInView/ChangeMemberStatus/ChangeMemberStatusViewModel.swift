8abdb6396a9e9b33e939e3a2d080d61d579fe3c2
binishmaharjan <legendbinish124@gmail.com>
2020-03-15 15:34:27 +0900

    adding new files

---
+++ Project_For_Binod/SignedInView/ChangeMemberStatus/ChangeMemberStatusViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeMemberStatusViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/03/15.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
---
+++ Project_For_Binod/TabBar/ProfileView/ChangeMemberStatus/ChangeMemberStatusViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeMemberStatusViewController.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/03/14.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
