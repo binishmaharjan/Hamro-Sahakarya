ccf7e0ddc00121ecc0a3251934d7c09bc6ae91f6
binishmaharjan <legendbinish124@gmail.com>
2020-04-08 11:36:40 +0900

    adding viewmodel file

---
+++ Project_For_Binod/SignedInView/ExtraAndExpensesView/ExtraAndExpensesViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ExtraAndExpensesViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/08.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct ExtraAndExpensesViewModel {
+    
+}
