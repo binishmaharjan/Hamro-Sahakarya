b5adcf32ff55a6abf8b2eb11d733e15150ecff04
binishmaharjan <legendbinish124@gmail.com>
2020-04-10 11:26:05 +0900

    bindiing state

--- Project_For_Binod/TabBar/ProfileView/ExtraAndExpenses/ExtraAndExpensesViewController.swift
+++ Project_For_Binod/TabBar/ProfileView/ExtraAndExpenses/ExtraAndExpensesViewController.swift
@@ -20,9 +20,12 @@ final class ExtraAndExpensesViewController: UIViewController {
     
     // MARK: Properties
     private var viewModel: ExtraAndExpensesViewModel!
+    private var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindApiState()
+        bindUIState()
     }
     
     // MARK: IBActions
@@ -38,5 +41,49 @@ extension ExtraAndExpensesViewController: StoryboardInstantiable {
         let viewController = ExtraAndExpensesViewController.loadFromStoryboard()
         viewController.viewModel = viewModel
         return viewController
+    }
+}
+
+// MARK: Bindable
+extension ExtraAndExpensesViewController {
+    
+    private func bindApiState() {
+        viewModel.apiState.driveNext { (state) in
+            switch state {
+            case .completed:
+                GUIManager.shared.stopAnimation()
+                
+            case .error(let error):
+                let dropDownModel = DropDownModel(dropDownType: .error, message: error.localizedDescription)
+                GUIManager.shared.showDropDownNotification(data: dropDownModel)
+                
+            case .loading:
+                GUIManager.shared.startAnimation()
+                
+            default:
+                break
+            }
+        }.disposed(by: disposeBag)
+    }
+    
+    private func bindUIState() {
+        // Input
+        amountTextField.rx.text
+            .asDriver()
+            .map { Int($0 ?? "0") ?? 0 }
+            .drive(viewModel.amountInput)
+            .disposed(by: disposeBag)
+        
+        reasonTextView.rx.text
+            .asDriver()
+            .map { $0 ?? "" }
+            .drive(viewModel.reasonInput)
+            .disposed(by: disposeBag)
+        
+        // Output
+        viewModel.isConfirmButtonEnabled
+            .asDriver(onErrorJustReturn: false)
+            .drive(confirmButton.rx.isEnabled)
+            .disposed(by: disposeBag)
     }
 }
