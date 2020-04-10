ffc8f9c5fbdbb27e0630064b6bab7fedee6a109e
binishmaharjan <legendbinish124@gmail.com>
2020-04-10 10:53:49 +0900

    adding string extension

---
+++ Project_For_Binod/String+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  String+Extension.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/10.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func isNotEmpty() -> Bool {
+        return !self.isEmpty
+    }
+}
