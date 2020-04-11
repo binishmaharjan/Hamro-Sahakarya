6438d6e85a0398db8941c12183daed48cc1d5ffb
binishmaharjan <legendbinish124@gmail.com>
2020-04-11 11:13:31 +0900

    adding view model file

---
+++ Project_For_Binod/SignedInView/LoanMember/LoanMemberViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  LoanMemberViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/11.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol LoanMemberViewModelProtocol {
+    
+}
+
+struct LoanMemberViewModel {
+    
+}
