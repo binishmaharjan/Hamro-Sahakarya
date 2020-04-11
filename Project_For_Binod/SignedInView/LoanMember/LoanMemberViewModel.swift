f9e823f37106158158f04afa4da4ec1db6506104
binishmaharjan <legendbinish124@gmail.com>
2020-04-11 11:33:26 +0900

    defining view model properties

--- Project_For_Binod/SignedInView/LoanMember/LoanMemberViewModel.swift
+++ Project_For_Binod/SignedInView/LoanMember/LoanMemberViewModel.swift
@@ -11,7 +11,18 @@ import RxSwift
 import RxCocoa
 
 protocol LoanMemberViewModelProtocol {
+    var loanAmount: BehaviorRelay<Int> { get }
+    var selectedMember: BehaviorRelay<UserProfile> { get }
+    var isLoanMemberButtonEnabled: Observable<Bool> { get }
     
+    var apiState: Driver<State> { get }
+    var loanMemberSuccessful: Driver<Bool> { get }
+    
+    func getAllMembers()
+    func loanMember()
+    
+    func numberOfRows() -> Int
+    func viewModelForRow(at indexPath: IndexPath) -> MemberCellViewModel
 }
 
 struct LoanMemberViewModel: LoanMemberViewModelProtocol {
