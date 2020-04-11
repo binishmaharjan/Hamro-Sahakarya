bed8a4697406050722fd1c1559b9105bd461826a
binishmaharjan <legendbinish124@gmail.com>
2020-04-11 10:11:16 +0900

    add files for loan view controller

---
+++ Project_For_Binod/TabBar/ProfileView/LoanMember/LoanMemberViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoanMemberViewController.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/11.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class LoanMemberViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
