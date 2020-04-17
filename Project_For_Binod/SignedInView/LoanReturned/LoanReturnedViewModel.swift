092999fda2036fb5027ac84926cba7604d80c823
binishmaharjan <legendbinish124@gmail.com>
2020-04-17 22:51:48 +0900

    creating files for viewmodel

---
+++ Project_For_Binod/SignedInView/LoanReturned/LoanReturnedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoanReturnedViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/17.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
+
+struct LoanReturnedViewModel {
+    
+}
