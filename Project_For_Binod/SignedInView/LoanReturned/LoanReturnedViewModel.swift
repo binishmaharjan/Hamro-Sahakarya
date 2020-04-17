39d85e15be522b128299f1fceca512492509b7a8
binishmaharjan <legendbinish124@gmail.com>
2020-04-17 23:22:51 +0900

    defining protocol and uistate protocol

--- Project_For_Binod/SignedInView/LoanReturned/LoanReturnedViewModel.swift
+++ Project_For_Binod/SignedInView/LoanReturned/LoanReturnedViewModel.swift
@@ -7,9 +7,36 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol LoanReturnedUIStateProtocol {
+    var selectedMember: UserProfile? { get }
+    var returnedAmount: Int { get }
+}
+
+extension LoanReturnedUIStateProtocol {
+     
+    var isReturnAmountButtonEnabled: Bool {
+        return selectedMember != nil && returnedAmount > 0
+    }
+}
 
 protocol LoanReturnedViewModelProtocol {
-    
+    var returnedAmount: BehaviorRelay<Int> { get }
+      var selectedMember: BehaviorRelay<UserProfile?> { get }
+      var isReturnAmountButtonEnabled: Observable<Bool> { get }
+      
+      var apiState: Driver<State> { get }
+      var returnedAmountSuccessful: Driver<Bool> { get }
+      
+      func getAllMembers()
+      func returnAmount()
+      
+      func numberOfRows() -> Int
+      func viewModelForRow(at indexPath: IndexPath) -> MemberCellViewModel
+      func userProfileForRow(at indexPath: IndexPath) -> UserProfile
+      func isUserSelected(userProfile: UserProfile) -> Bool
 }
 
 struct LoanReturnedViewModel: LoanReturnedViewModelProtocol {
