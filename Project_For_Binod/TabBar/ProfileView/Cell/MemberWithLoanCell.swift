da07af2de275d76c121c22813944617caa560187
binishmaharjan <legendbinish124@gmail.com>
2020-04-18 14:10:13 +0900

    adding files for member with loan cell

---
+++ Project_For_Binod/TabBar/ProfileView/Cell/MemberWithLoanCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemberWithLoanCell.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/18.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+
+class MemberWithLoanCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
