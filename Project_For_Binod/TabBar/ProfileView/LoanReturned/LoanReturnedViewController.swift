d4b145b3cc30e5f42fe82430e5326f68c16f0a47
binishmaharjan <legendbinish124@gmail.com>
2020-04-18 12:01:08 +0900

    binding state

--- Project_For_Binod/TabBar/ProfileView/LoanReturned/LoanReturnedViewController.swift
+++ Project_For_Binod/TabBar/ProfileView/LoanReturned/LoanReturnedViewController.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class LoanReturnedViewController: UIViewController {
     
@@ -16,10 +18,24 @@ final class LoanReturnedViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     
     private var viewModel: LoanReturnedViewModelProtocol!
+    private let disposeBag = DisposeBag()
+    private var addButton: UIBarButtonItem!
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupBarButton()
+        bindApiState()
+        bindState()
+    }
+    
+    private func setupBarButton() {
+        addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
+        navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc private func addButtonPressed() {
+        viewModel.returnAmount()
     }
 }
 
@@ -30,5 +46,57 @@ extension LoanReturnedViewController: StoryboardInstantiable {
         let viewController = LoanReturnedViewController.loadFromStoryboard()
         viewController.viewModel = viewModel
         return viewController
+    }
+}
+
+
+// MARK: Binding
+extension LoanReturnedViewController {
+    private func bindApiState() {
+        
+        viewModel.apiState
+            .withLatestFrom(viewModel.returnedAmountSuccessful) { return ($0, $1) }
+            .driveNext { [weak self] (state, returnedSuccessful) in
+                
+                switch state {
+                case .completed:
+                    if returnedSuccessful {
+                        let dropDownModel = DropDownModel(dropDownType: .success, message: "Successful!!!")
+                        GUIManager.shared.showDropDownNotification(data: dropDownModel)
+                        
+                        self?.amountTextField.text = ""
+                    }
+                        
+                    self?.tableView.reloadData()
+                                    
+                    GUIManager.shared.stopAnimation()
+                    
+                case .error(let error):
+                    let dropDownModel = DropDownModel(dropDownType: .error, message: error.localizedDescription)
+                    GUIManager.shared.showDropDownNotification(data: dropDownModel)
+                    
+                case .loading:
+                    GUIManager.shared.startAnimation()
+                    
+                default:
+                    break
+                }
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    private func bindState() {
+        // Input
+        amountTextField.rx.text
+            .asDriver()
+            .map { Int($0 ?? "0") ?? 0 }
+            .drive(viewModel.returnedAmount)
+            .disposed(by: disposeBag)
+        
+        // Output
+        viewModel.isReturnAmountButtonEnabled
+            .asDriver(onErrorJustReturn: false)
+            .drive(addButton.rx.isEnabled)
+            .disposed(by: disposeBag)
     }
 }
