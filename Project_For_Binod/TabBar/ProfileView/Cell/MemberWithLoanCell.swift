b76b62112b69d2f668273005f495cd3955bc6967
binishmaharjan <legendbinish124@gmail.com>
2020-04-18 14:11:24 +0900

    binding cell with viewmodel

--- Project_For_Binod/TabBar/ProfileView/Cell/MemberWithLoanCell.swift
+++ Project_For_Binod/TabBar/ProfileView/Cell/MemberWithLoanCell.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class MemberWithLoanCell: UITableViewCell {
-
+    
+    @IBOutlet weak var memberImageView: UIImageView!
+    @IBOutlet weak var memberNameLabel: UILabel!
+    @IBOutlet weak var loanAmountLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        memberImageView.layer.cornerRadius = memberImageView.width / 2
+        memberImageView.clipsToBounds = true
     }
     
+    func bind(viewModel: MemberWithLoanViewModelProtocol) {
+        memberImageView.loadImage(with: viewModel.imageUrl)
+        memberNameLabel.text = viewModel.fullname
+        loanAmountLabel.text = viewModel.loanAmount
+    }
+}
 // MARK: ViewModel
 protocol MemberWithLoanViewModelProtocol {
   var imageUrl: URL? { get }
