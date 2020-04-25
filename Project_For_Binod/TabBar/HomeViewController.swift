93bdfc4e37cd62b93900e2f60329bf6f09da92e5
binishmaharjan <legendbinish124@gmail.com>
2020-04-25 16:57:07 +0900

    confirming home view controller to storyboard instantialble

--- Project_For_Binod/TabBar/HomeViewController.swift
+++ Project_For_Binod/TabBar/HomeViewController.swift
@@ -7,27 +7,25 @@
 //
 
 import UIKit
+import RxSwift
 
 final class HomeViewController: UIViewController {
-  
-  init() {
-    super.init(nibName: nil, bundle: nil)
-    setup()
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = .orange
-  }
-  
-  private func setup() {
-    title = nil
-    tabBarItem.image = UIImage(named: "icon_home")?.withRenderingMode(.alwaysOriginal)
-    tabBarItem.selectedImage = UIImage(named: "icon_home_h")?.withRenderingMode(.alwaysOriginal)
-    tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-  }
+    
+    private var viewModel: HomeViewModelProtocol!
+    private let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+// MARK: Storyboard Instantiable
+extension HomeViewController: StoryboardInstantiable {
+    
+    static func makeInstance(viewModel: HomeViewModelProtocol) -> HomeViewController {
+        let viewController = loadFromStoryboard()
+        viewController.viewModel = viewModel
+        return viewController
+    }
 }
