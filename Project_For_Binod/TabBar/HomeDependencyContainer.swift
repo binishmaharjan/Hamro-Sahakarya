183717d09d10fbe3d466e84d2f1b6e9fd5b90791
binishmaharjan <legendbinish124@gmail.com>
2020-04-25 15:17:30 +0900

    create home dependency container file

---
+++ Project_For_Binod/TabBar/HomeDependencyContainer.swift
@@ -0,0 +1,9 @@
+//
+//  HomeDependencyContainer.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/25.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
