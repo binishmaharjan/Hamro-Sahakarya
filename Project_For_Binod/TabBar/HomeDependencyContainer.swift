4c18c0c655976a7ca0e6ee11a5a153290a93ce4e
binishmaharjan <legendbinish124@gmail.com>
2020-04-25 16:58:40 +0900

    initializing home depedency container

--- Project_For_Binod/TabBar/HomeDependencyContainer.swift
+++ Project_For_Binod/TabBar/HomeDependencyContainer.swift
@@ -10,4 +10,30 @@ import Foundation
 
 final class HomeDependencyContainer {
     
+    //MARK: Properties
+    private let sharedUserSessionRepository: UserSessionRepository
+    private let sharedMainViewModel: MainViewModel
+    private let userSession: UserSession
+    
+    private let homeMainViewModel: HomeMainViewModel
+    
+    // MARK: Init
+    init(dependecyContainer: SignedInDepedencyConatiner, userSession: UserSession) {
+        func makeHomeMainViewModel() -> HomeMainViewModel {
+            return HomeMainViewModel()
+        }
+        
+        self.sharedUserSessionRepository = dependecyContainer.sharedUserSessionRepository
+        self.sharedMainViewModel = dependecyContainer.sharedMainViewModel
+        self.userSession = userSession
+        
+        self.homeMainViewModel = makeHomeMainViewModel()
+    }
+}
+
+
+protocol HomeViewControllerFactory {
+    
+    func makeHomeViewController() -> HomeViewController
+}
 }
