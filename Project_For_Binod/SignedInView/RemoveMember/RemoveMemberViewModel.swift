2b4436ba2e44cc310dc3e72eeb3988f447ad57b8
binishmaharjan <legendbinish124@gmail.com>
2020-04-29 11:26:04 +0900

    adding files for remove member

---
+++ Project_For_Binod/SignedInView/RemoveMember/RemoveMemberViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RemoveMemberViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/29.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
+
+struct RemoveMemberViewModel {
+    
+}
---
+++ Project_For_Binod/TabBar/ProfileView/RemoveMemberViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RemoveMemberViewController.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/04/29.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+
+final class RemoveMemberViewController: UIViewController {
+    
+}
