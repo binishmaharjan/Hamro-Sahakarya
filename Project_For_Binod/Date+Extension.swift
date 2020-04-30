2178cfad932cd8416b5e43aab369c95ed053e065
binishmaharjan <legendbinish124@gmail.com>
2020-04-30 13:19:06 +0900

    adding date extension

--- Project_For_Binod/Date+Extension.swift
+++ Project_For_Binod/Date+Extension.swift
@@ -18,13 +18,20 @@ extension Date {
     formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
     return formatter.string(from: self)
   }
-  
+    
+    var toGegorianMonthDateYearString: String {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.dateFormat = "MMM dd, yyyy"
+        return formatter.string(from: self)
+    }
+    
 }
 
 // MARK: String To Date
 extension String {
   
-  var toDateTimeAndTime: Date {
+  var toDateAndTime: Date {
     let formatter = DateFormatter()
     formatter.timeZone = TimeZone(secondsFromGMT: 0)
     formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
