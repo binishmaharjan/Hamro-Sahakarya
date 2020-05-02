2c89e12c149282a9bbdfa790b3e82201478700ac
binishmaharjan <legendbinish124@gmail.com>
2020-05-02 15:21:53 +0900

    add refresh function

--- Project_For_Binod/TabBar/LogTab/LogViewController.swift
+++ Project_For_Binod/TabBar/LogTab/LogViewController.swift
@@ -18,17 +18,19 @@ final class LogViewController: UIViewController {
     
     private var viewModel: LogViewModel!
     private let disposeBag = DisposeBag()
+    private var refreshButton: UIBarButtonItem!
     
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         bindApiState()
+        setupBarButton()
         
         tableView.registerXib(of: LogViewCell.self)
         tableView.delegate = self
         tableView.dataSource = self
-        viewModel.loadLogs(isFirstLoad: true)
+        viewModel.fetchLogs(isRefresh: false)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,6 +45,15 @@ final class LogViewController: UIViewController {
         tabBarItem.selectedImage = UIImage(named: "icon_log_h")?.withRenderingMode(.alwaysOriginal)
         tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
     }
+    
+    private func setupBarButton() {
+        refreshButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshButtonPressed))
+        navigationItem.rightBarButtonItem = refreshButton
+    }
+    
+    @objc private func refreshButtonPressed() {
+        viewModel.fetchLogs(isRefresh: true)
+    }
 }
 
 // MARK: Bind
@@ -50,18 +61,25 @@ extension LogViewController {
     
     private func bindApiState() {
         viewModel.state
-            .drive(onNext: { [weak self] (state) in
+            .withLatestFrom(viewModel.isRefreshing) { return ($0, $1) }
+            .drive(onNext: { [weak self] (state, isRefreshing) in
                 switch state {
                 case .idle:
                     break
                 case .completed:
                     self?.tableView.reloadData()
                     GUIManager.shared.stopAnimation()
+                    
+                    if isRefreshing {
+                        self?.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
+                    }
+                    
                 case .error(let error):
                     GUIManager.shared.stopAnimation()
                     
                     let dropDownModel = DropDownModel(dropDownType: .error, message: error.localizedDescription)
                     GUIManager.shared.showDropDownNotification(data: dropDownModel)
+                    
                 case .loading:
                     GUIManager.shared.startAnimation()
                 }
