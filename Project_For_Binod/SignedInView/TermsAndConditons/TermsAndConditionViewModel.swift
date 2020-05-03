7670d80eb0dea2f7db1937ee92d4d8bcddf2b3e8
binishmaharjan <legendbinish124@gmail.com>
2020-05-03 11:30:27 +0900

    adding files

---
+++ Project_For_Binod/SignedInView/TermsAndConditons/TermsAndConditionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TermsAndConditionViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/03.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
+
+struct TermsAndConditionViewModel {
+    
+}
---
+++ Project_For_Binod/TabBar/ProfileView/TermsAndCondition/TermsAndConditionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TermsAndConditionViewController.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/03.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+
+final class TermsAndConditionViewController: UIViewController {
+    
+}
