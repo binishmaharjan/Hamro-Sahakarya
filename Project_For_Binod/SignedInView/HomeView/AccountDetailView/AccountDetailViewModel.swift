0891dcaad4608f6ab915c40a347ae9becd3bb124
binishmaharjan <legendbinish124@gmail.com>
2020-05-06 13:01:55 +0900

    adding files

---
+++ Project_For_Binod/SignedInView/HomeView/AccountDetailView/AccountDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AccountDetailViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/06.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
---
+++ Project_For_Binod/TabBar/AccountDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountDetailIView.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/05.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+
+final class AccountDetailView: UIView {
+    
+}
+
+extension AccountDetailView {
+    
+    static func makeInstance() {
+        let view = Acc
+    }
+}
