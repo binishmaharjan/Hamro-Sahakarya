cac8a07366f2718b8da2b6c319c64a73e09ade25
binishmaharjan <legendbinish124@gmail.com>
2020-05-09 12:51:01 +0900

    adding files for member graph

---
+++ Project_For_Binod/SignedInView/HomeView/MemberGraph/MemberGraphViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MemberGraphViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/09.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
+
+struct MemberGraphViewModel {
+    
+}
---
+++ Project_For_Binod/TabBar/MemberGraphView.swift
@@ -0,0 +1,13 @@
+//
+//  MemberGraphView.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/09.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import UIKit
+
+final class MemberGraphView: UIView {
+    
+}
