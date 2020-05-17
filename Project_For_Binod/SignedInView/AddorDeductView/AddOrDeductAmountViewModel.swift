3a6700ef9aa80cd41ec7f6fdf3281ca7b9dd61f2
binishmaharjan <legendbinish124@gmail.com>
2020-05-17 16:58:51 +0900

    adding files

---
+++ Project_For_Binod/SignedInView/AddorDeductView/AddOrDeductAmountViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AddOrDeductAmountViewModel.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/17.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
---
+++ Project_For_Binod/TabBar/ProfileView/AddOrDeductAmount/AddOrDeductAmountViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddOrDeductAmountViewController.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/17.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
