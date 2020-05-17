bfa733d3561b8cedaab1e6c27362ab1052c05243
binishmaharjan <legendbinish124@gmail.com>
2020-05-17 17:09:45 +0900

    making storyboard instantiable and returning associated view

--- Project_For_Binod/TabBar/ProfileView/AddOrDeductAmount/AddOrDeductAmountViewController.swift
+++ Project_For_Binod/TabBar/ProfileView/AddOrDeductAmount/AddOrDeductAmountViewController.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2020 JEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class AddOrDeductAmountViewController: UIViewController {
+    
+    //MARK: Properties
+    private var viewModel: AddOrDeductAmountViewModelProtocol!
+    
+    //MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+//MARK: Storyboard Instantiable
+extension AddOrDeductAmountViewController: StoryboardInstantiable {
+    
+    static func makeInstance(viewModel: AddOrDeductAmountViewModelProtocol) -> AddOrDeductAmountViewController {
+        let viewController = AddOrDeductAmountViewController.loadFromStoryboard()
+        viewController.viewModel = viewModel
+        return viewController
+    }
+}
+
+//MARK: Associate View
+extension AddOrDeductAmountViewController: ViewControllerWithAssociatedView {
+    
+    func getAssociateView() -> ProfileMainView {
+        return .addOrDeductAmount
+    }
+}
