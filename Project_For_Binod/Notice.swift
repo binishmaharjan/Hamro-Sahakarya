56dbdcb758e07fab1abd29ec6b3155817a405ef4
binishmaharjan <legendbinish124@gmail.com>
2020-05-24 16:21:02 +0900

    adding Notice model

---
+++ Project_For_Binod/Notice.swift
@@ -0,0 +1,14 @@
+//
+//  Notice.swift
+//  Project_For_Binod
+//
+//  Created by Maharjan Binish on 2020/05/24.
+//  Copyright © 2020 JEC. All rights reserved.
+//
+
+import Foundation
+
+struct Notice: Codable {
+    let message: String
+    let admin: String
+}
