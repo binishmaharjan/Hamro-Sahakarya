80bfcb64476ea4cfe60064611032ccc66728ba58
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-02-25 01:35:35 +0900

    create add monthly fee_first commit

---
+++ App/HamroSahakaryaV2/Sources/ProfileFeatureV2/Core/AddMonthlyFee.swift
@@ -0,0 +1,172 @@
+import Foundation
+import ComposableArchitecture
+import SharedModels
+import UserApiClient
+import SwiftHelpers
+
+@Reducer
+public struct AddMonthlyFee {
+    @ObservableState
+    public struct State: Equatable {
+        public enum Field: Equatable {
+            case amount
+        }
+        
+        public init(admin: User) {
+            self.admin = admin
+        }
+        public init(admin: User = .mock, members: [User]) {
+            self.admin = admin
+            self.members = members
+        }
+        
+        @Presents var destination: Destination.State?
+        var admin: User
+        var isLoading: Bool = false
+        var amount: String = ""
+        var members: [User] = []
+        var selectedMembers: [User] = []
+        var focusedField: Field? = .amount
+        var isValidInput: Bool {
+            return amount.int > 0
+        }
+    }
+    
+    public enum Action: BindableAction, Equatable {
+        public enum SelectionType: Equatable {
+            case all
+            case member(User)
+        }
+        case destination(PresentationAction<Destination.Action>)
+        case binding(BindingAction<State>)
+        
+        case onAppear
+        case membersListResponse(TaskResult<[User]>)
+        case rowSelected(SelectionType)
+        case addMonthlyFeeTapped
+        case addMonthlyResponse(TaskResult<VoidSuccess>)
+    }
+    
+    public init() { }
+    
+    @Dependency(\.userApiClient) private var userApiClient
+    
+    public var body: some ReducerOf<Self> {
+        BindingReducer()
+        
+        Reduce<State, Action> { state, action in
+            switch action {
+            case .onAppear:
+                state.isLoading = true
+                return .run { send in
+                    await send(
+                        .membersListResponse(
+                            TaskResult {
+                                try await userApiClient.fetchAllMembers()
+                            }
+                        )
+                    )
+                }
+                
+            case .membersListResponse(.success(let members)):
+                state.isLoading = false
+                state.members = members
+                return .none
+                
+            case .membersListResponse(.failure(let error)):
+                state.isLoading = false
+                state.destination = .alert(.onError(error))
+                return .none
+                
+            case .rowSelected(.all):
+                state.selectedMembers = []
+                return .none
+                
+            case .rowSelected(.member(let member)):
+                if let index = state.selectedMembers.firstIndex(of: member) {
+                    state.selectedMembers.remove(at: index)
+                } else {
+                    state.selectedMembers.append(member)
+                }
+                return .none
+                
+            case .addMonthlyFeeTapped:
+                state.isLoading = true
+                return .run { [state = state] send in
+                    await send(
+                        .addMonthlyResponse(
+                            TaskResult {
+                                try await addMonthlyFee(
+                                    admin: state.admin,
+                                    members: state.members,
+                                    amount: state.amount.int
+                                )
+                            }
+                        )
+                    )
+                }
+                
+            case .addMonthlyResponse(.success):
+                state.isLoading = false
+                state.amount = ""
+                state.destination = .alert(.onUpdateSuccessful())
+                return .none
+                
+            case .addMonthlyResponse(.failure(let error)):
+                state.isLoading = false
+                state.destination = .alert(.onError(error))
+                return .none
+                
+            case .binding, .destination:
+                return .none
+            }
+        }
+        .ifLet(\.$destination, action: \.destination) {
+            Destination()
+        }
+    }
+}
+
+// MARK: Apis
+extension AddMonthlyFee {
+    /// Add monthly fee for multiple users. (Hit multiple api in parallel)
+    private func addMonthlyFee(admin: User, members: [User], amount: Balance) async throws -> VoidSuccess {
+        // Create a task group to run multiple apis
+        try await withThrowingTaskGroup(of: Void.self) { group in
+            for member in members {
+                group.addTask {
+                    // Hit multiple apis
+                    try await userApiClient.addMonthlyFee(admin, member, amount)
+                }
+            }
+            // Wait for all task to complete
+            for try await _ in group { }
+            
+            return VoidSuccess()
+        }
+    }
+}
+
+// MARK: Destination
+extension AddMonthlyFee {
+    @Reducer
+    public struct Destination {
+        public enum State: Equatable {
+            case alert(AlertState<Action.Alert>)
+        }
+        
+        public enum Action: Equatable {
+            public enum Alert: Equatable {}
+            
+            case alert(Alert)
+        }
+        
+        public init() { }
+        
+        public var body: some ReducerOf<Self> {
+            Reduce<State, Action> { state, action in
+                return .none
+            }
+        }
+    }
+}
---
+++ App/HamroSahakaryaV2/Sources/ProfileFeatureV2/Views/AddMonthlyFeeView.swift
@@ -0,0 +1,108 @@
+import SwiftUI
+import ComposableArchitecture
+import SharedUIs
+
+public struct AddMonthlyFeeView: View {
+    public init(store: StoreOf<AddMonthlyFee>) {
+        self.store = store
+    }
+    
+    @FocusState private var focusedField: AddMonthlyFee.State.Field?
+    @Bindable private var store: StoreOf<AddMonthlyFee>
+    
+    public var body: some View {
+        VStack(spacing: 24) {
+            VStack(alignment: .leading) {
+                Text(#localized("Amount"))
+                    .font(.customSubHeadline)
+                    .foregroundStyle(#color("secondary"))
+                
+                TextField(#localized("Amount"), text: $store.amount)
+                    .textFieldStyle(.icon(#img("icon_yen")))
+                    .keyboardType(.numberPad)
+                    .focused($focusedField, equals: .amount)
+            }
+            VStack {
+                Text(#localized("Select Target Members"))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.customSubHeadline2)
+                    .foregroundStyle(#color("gray"))
+                
+                ScrollView() {
+                    MemberSelectionItemView(label: #localized("All Members")) {
+                        store.send(.rowSelected(.all))
+                    }
+                    
+                    separator
+                    
+                    VStack(spacing: 0) {
+                        ForEach(store.state.members) { member in
+                            MemberSelectionItemView(label: member.username) {
+                                store.send(.rowSelected(.member(member)))
+                            }
+                        }
+                    }
+                    .background(#color("white"))
+                    .mask(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+            }
+            
+            addMonthlyFeeButton
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(20)
+        .padding()
+        .bind($store.focusedField, to: self.$focusedField)
+        .background(#color("background"))
+        .customNavigationBar(#localized("Add Monthly Fee"))
+        .loadingView(store.isLoading)
+        .onAppear { store.send(.onAppear) }
+        .alert(
+            $store.scope(state: \.destination?.alert, action: \.destination.alert)
+        )
+    }
+}
+
+// MARK: View Parts
+extension AddMonthlyFeeView {
+    private var addMonthlyFeeButton: some View {
+        Button {
+            store.send(.addMonthlyFeeTapped)
+        } label: {
+            HStack {
+                Image(systemName: "arrow.right")
+                Text(#localized("Add Monthly Fee"))
+                    .font(.customHeadline)
+            }
+            .largeButton()
+        }
+        .disabled(!store.isValidInput)
+    }
+    
+    private var separator: some View {
+        HStack {
+            Rectangle()
+                .frame(height: 1)
+                .opacity(0.1)
+            
+            Text(#localized("OR"))
+                .font(.customSubHeadline2)
+                .foregroundStyle(#color("black").opacity(0.3))
+            
+            Rectangle()
+                .frame(height: 1)
+                .opacity(0.1)
+        }
+    }
+}
+
+#Preview {
+    AddMonthlyFeeView(
+        store: .init(
+            initialState: .init(members: [.mock ,.mock2]),
+            reducer: AddMonthlyFee.init
+        )
+    )
+}
