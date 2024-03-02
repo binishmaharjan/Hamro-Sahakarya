c76fb5e2fb79a3b63d557e925aecf185728bbc22
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-03-02 14:39:55 +0900

    implement loan returned

--- App/HamroSahakaryaV2/Sources/ProfileFeatureV2/Core/LoanReturned.swift
+++ App/HamroSahakaryaV2/Sources/ProfileFeatureV2/Core/LoanReturned.swift
@@ -6,22 +6,127 @@ import SharedModels
 public struct LoanReturned {
     @ObservableState
     public struct State: Equatable {
+        public enum Field: Equatable {
+            case amount
+        }
         public init(admin: User) {
             self.admin = admin
         }
         
+        @Presents var destination: Destination.State?
         var admin: User
+        var memberSelect: MemberSelect.State = MemberSelect.State(members: [], mode: .membersOnly)
+        var isLoading: Bool = false
+        var amount: String = ""
+        var members: [User] = []
+        var focusedField: Field? = .amount
+        var isValidInput: Bool {
+            return amount.int > 0
+        }
     }
     
-    public enum Action {
+    public enum Action: BindableAction {
+        case destination(PresentationAction<Destination.Action>)
+        case binding(BindingAction<State>)
+        case memberSelect(MemberSelect.Action)
         
+        case onAppear
+        case membersListResponse(Result<[User], Error>)
+        case loanReturnedTapped
+        case loanReturnedResponse(Result<Void, Error>)
     }
     
     public init() { }
     
+    @Dependency(\.userApiClient) private var userApiClient
+    
     public var body: some ReducerOf<Self> {
+        BindingReducer()
+        
+        Scope(state: \.memberSelect, action: \.memberSelect) {
+            MemberSelect()
+        }
+        
         Reduce<State, Action> { state, action in
-            return .none
+            switch action {
+            case .onAppear:
+                guard state.members.isEmpty else { return .none }
+                state.isLoading = true
+                return .run { send in
+                    await send(
+                        .membersListResponse(
+                            Result {
+                                try await userApiClient.fetchAllMembersWithLoan()
+                            }
+                        )
+                    )
+                }
+                
+            case .membersListResponse(.success(let members)):
+                state.isLoading = false
+                state.members = members
+                state.memberSelect = MemberSelect.State(members: members, mode: .membersOnly)
+                return .none
+                
+            case .membersListResponse(.failure(let error)):
+                state.isLoading = false
+                state.destination = .alert(.onError(error))
+                return .none
+                
+            case .loanReturnedTapped:
+                state.isLoading = true
+                return .run { [state = state] send in
+                    await send(
+                        .loanReturnedResponse(
+                            Result {
+                                try await userApiClient.loanReturned(
+                                    state.admin,
+                                    state.memberSelect.selectedMembers[0],
+                                    state.amount.int
+                                )
+                            }
+                        )
+                    )
+                }
+                
+            case .loanReturnedResponse(.success):
+                state.isLoading = false
+                state.amount = ""
+                state.destination = .alert(.onUpdateSuccessful())
+                return .none
+                
+            case .loanReturnedResponse(.failure(let error)):
+                state.isLoading = false
+                state.destination = .alert(.onError(error))
+                return .none
+                
+            case .binding, .destination, .memberSelect:
+                return .none
+            }
+        }
+    }
+}
+
+// MARK: Destination
+extension LoanReturned {
+    @Reducer
+    public struct Destination {
+        public enum State: Equatable {
+            case alert(AlertState<Action.Alert>)
+        }
+        
+        public enum Action {
+            public enum Alert: Equatable {}
+            
+            case alert(Alert)
+        }
+        
+        public init() { }
+        
+        public var body: some ReducerOf<Self> {
+            Reduce<State, Action> { state, action in
+                return .none
+            }
         }
     }
 }
