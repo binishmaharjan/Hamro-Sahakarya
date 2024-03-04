ac9ca8392cadc5a3422ba82afb79d4aea0459bed
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2024-03-04 16:58:58 +0900

    implement remove member

--- App/HamroSahakaryaV2/Sources/ProfileFeatureV2/Core/RemoveMember.swift
+++ App/HamroSahakaryaV2/Sources/ProfileFeatureV2/Core/RemoveMember.swift
@@ -1,6 +1,8 @@
 import Foundation
 import ComposableArchitecture
 import SharedModels
+import UserApiClient
+import SharedUIs
 
 @Reducer
 public struct RemoveMember {
@@ -10,18 +12,169 @@ public struct RemoveMember {
             self.admin = admin
         }
         
+        @Presents var destination: Destination.State?
         var admin: User
+        var members: [User] = []
+        var memberSelect: MemberSelect.State = MemberSelect.State(members: [], mode: .membersOnly)
+        var isLoading: Bool = false
+        var isRemovingMember: Bool = false
+        var isValidInput: Bool {
+            return !memberSelect.selectedMembers.isEmpty
+        }
     }
     
     public enum Action {
+        case destination(PresentationAction<Destination.Action>)
+        case memberSelect(MemberSelect.Action)
         
+        case onAppear
+        case membersListResponse(Result<[User], Error>)
+        case removeMemberTapped
+        case removeMemberResponse(Result<Void, Error>)
     }
     
     public init() { }
     
+    @Dependency(\.userApiClient) private var userApiClient
+    
     public var body: some ReducerOf<Self> {
+        Scope(state: \.memberSelect, action: \.memberSelect) {
+            MemberSelect()
+        }
+        
         Reduce<State, Action> { state, action in
-            return .none
+            switch action {
+            case.onAppear:
+                guard state.members.isEmpty else { return .none }
+                state.isLoading = true
+                return .run { send in
+                    await send(
+                        .membersListResponse(
+                            Result {
+                                try await userApiClient.fetchAllMembers()
+                            }
+                        )
+                    )
+                }
+                
+            case .membersListResponse(.success(let members)):
+                state.isLoading = false
+                state.members = members
+                state.memberSelect = MemberSelect.State(members: members, mode: .membersOnly)
+
+                // if refetch flag after the removing member is true, show alert
+                if state.isRemovingMember {
+                    state.isRemovingMember = true
+                    state.destination = .alert(.onMemberRemoved())
+                }
+                
+                return .none
+                
+            case .membersListResponse(.failure(let error)):
+                state.isLoading = false
+                state.destination = .alert(.onError(error))
+                return .none
+                
+            case .removeMemberTapped:
+                guard !state.memberSelect.selectedMembers.isEmpty else { return .none }
+                state.destination = .alert(.removeMemberConfirmation())
+                return .none
+                
+            case .destination(.presented(.alert(.removeConfirmationTapped))):
+                guard !state.memberSelect.selectedMembers.isEmpty else { return .none }
+                let targetMember = state.memberSelect.selectedMembers[0]
+                state.isLoading = true
+                return .run { [admin = state.admin] send in
+                    await send(
+                        .removeMemberResponse(
+                            Result {
+                                try await userApiClient.removeMember(
+                                    admin,
+                                    targetMember
+                                )
+                            }
+                        )
+                    )
+                }
+                
+            case .removeMemberResponse(.success):
+                // Creating flag to show alert after refetching member
+                state.isRemovingMember = true
+                return .run { send in
+                    await send(
+                        .membersListResponse(
+                            Result {
+                                try await userApiClient.fetchAllMembers()
+                            }
+                        )
+                    )
+                }
+                
+            case .removeMemberResponse(.failure(let error)):
+                state.isLoading = false
+                state.destination = .alert(.onError(error))
+                return .none
+                
+            case .destination, .memberSelect:
+                return .none
+            }
+        }
+        .ifLet(\.$destination, action: \.destination) {
+            Destination()
+        }
+    }
+}
+
+// MARK: Destination
+extension RemoveMember {
+    @Reducer
+    public struct Destination {
+        public enum State: Equatable {
+            case alert(AlertState<Action.Alert>)
+        }
+        
+        public enum Action {
+            public enum Alert: Equatable {
+                case removeConfirmationTapped
+            }
+            
+            case alert(Alert)
+        }
+        
+        public init() { }
+        
+        public var body: some ReducerOf<Self> {
+            Reduce<State, Action> { state, action in
+                return .none
+            }
+        }
+    }
+}
+
+// MARK: AlertState
+extension AlertState where Action == RemoveMember.Destination.Action.Alert {
+    static func removeMemberConfirmation() -> AlertState {
+        AlertState {
+            TextState(#localized("Remove Confirmation"))
+        } actions: {
+            ButtonState(role: .cancel) {
+                TextState(#localized("Cancel"))
+            }
+            ButtonState(action: .removeConfirmationTapped) {
+                TextState(#localized("Yes, Remove Member"))
+            }
+        } message: {
+            TextState(#localized("Are you sure you want to remove this member? This action is irreversible and member data will be completed removed."))
+        }
+    }
+    
+    public static func onMemberRemoved() -> AlertState {
+        AlertState {
+            TextState(#localized("Success"))
+        } actions: {
+            ButtonState { TextState(#localized("Ok")) }
+        } message: {
+            TextState(#localized("The member has been removed successfully"))
         }
     }
 }
--- App/HamroSahakaryaV2/Sources/ProfileFeatureV2/Views/RemoveMemberView.swift
+++ App/HamroSahakaryaV2/Sources/ProfileFeatureV2/Views/RemoveMemberView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import ComposableArchitecture
+import SharedUIs
 
 public struct RemoveMemberView: View {
     public init(store: StoreOf<RemoveMember>) {
@@ -9,7 +10,46 @@ public struct RemoveMemberView: View {
     @Bindable private var store: StoreOf<RemoveMember>
     
     public var body: some View {
-        Text("Remove Member")
+        VStack(spacing: 24) {
+            VStack {
+                Text(#localized("Select Target Members"))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.customSubHeadline2)
+                    .foregroundStyle(#color("gray"))
+                
+                MemberSelectView(store: store.scope(state: \.memberSelect, action: \.memberSelect))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            
+            removeButton
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(20)
+        .padding()
+        .background(#color("background"))
+        .customNavigationBar(#localized("Remove Member"))
+        .loadingView(store.isLoading)
+        .onAppear { store.send(.onAppear) }
+        .alert(
+            $store.scope(state: \.destination?.alert, action: \.destination.alert)
+        )
+    }
+}
+
+// MARK: View Parts
+extension RemoveMemberView {
+    private var removeButton: some View {
+        Button {
+            store.send(.removeMemberTapped)
+        } label: {
+            HStack {
+                Image(systemName: "arrow.right")
+                Text(#localized("Remove Member"))
+                    .font(.customHeadline)
+            }
+            .largeButton()
+        }
+        .disabled(!store.isValidInput)
     }
 }
 
